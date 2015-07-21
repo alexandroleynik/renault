55d29cdbbc8362a10e35c8d8af724e46c02c1961
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-07-21 09:36:19 +0300

    disable debug module

--- common/config/web.php
+++ common/config/web.php
@@ -14,11 +14,11 @@ $config = [
 ];
 
 if (YII_DEBUG) {
-    $config['bootstrap'][]      = 'debug';
+    /*$config['bootstrap'][]      = 'debug';
     $config['modules']['debug'] = [
         'class'      => 'yii\debug\Module',
         'allowedIPs' => ['127.0.0.1', '::1', '192.168.33.1'],
-    ];
+    ];*/
 }
 
 if (YII_ENV_DEV) {
