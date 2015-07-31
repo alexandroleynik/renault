03700b39cb00b02614c8a0ca633f4a2ddce6303f
Jar-L <yaros.lin@gmail.com>
2015-07-31 14:54:27 +0300

    contact page

--- frontend/web/js/app/widgets/contact/widget.js
+++ frontend/web/js/app/widgets/contact/widget.js
@@ -19,6 +19,138 @@
         
         loadTemplate(data);
     }
+	
+	    function mapInitialize(conf) {
+        // default options
+        var myLatlng1 = new google.maps.LatLng(49.3159955, 32.0068446);
+        var zoom = 6;
+
+        if (conf) {
+            app.logger.var(conf);
+        }
+        //if custom center
+        if (!$.isEmptyObject(conf) && !$.isEmptyObject(conf.center)) {
+            myLatlng1 = new google.maps.LatLng(conf.center.split(',')[0], conf.center.split(',')[1]);
+        }
+
+        //if custom zoom
+        if (!$.isEmptyObject(conf) && conf.zoom) {
+            zoom = conf.zoom;
+        }
+
+        // Map options
+        var mapOptions1 = {
+            scrollwheel: false,
+            center: myLatlng1,
+            zoom: zoom,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        // Init map
+        var map1 = new google.maps.Map(document.getElementById('mapresult'), mapOptions1);
+
+
+        // Create the search box and link it to the UI element.
+
+        markers = [];
+        var input = /** @type {HTMLInputElement} */(document.getElementById('test-drive-form-map-input-search'));
+        //map1.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        var searchBox = new google.maps.places.SearchBox((input));
+
+        // Listen for the event fired when the user selects an item from the
+        // pick list. Retrieve the matching places for that item.
+        google.maps.event.addListener(searchBox, 'places_changed', function () {
+            var places = searchBox.getPlaces();
+
+            if (places.length == 0) {
+                return;
+            }
+            for (var i = 0, marker; marker = markers[i]; i++) {
+                marker.setMap(null);
+            }
+
+            //filter dealers list
+            if (!$.isEmptyObject(searchBox.getPlaces()) && !$.isEmptyObject(searchBox.getPlaces()[0]) && !$.isEmptyObject(searchBox.getPlaces()[0].name)) {
+                var town = searchBox.getPlaces()[0].name;
+                app.logger.var(searchBox.getPlaces()[0]);
+                app.logger.text(town);
+
+                var filterValue = '.' + toCodeValue(town);
+                app.logger.text(filterValue);
+                // use filterFn if matches value
+                if (!$.isEmptyObject(app.view.$grid)) {
+                    app.view.$grid.isotope({filter: filterValue});
+                }
+
+                app.view.mapFilterValue = filterValue;
+
+            }
+
+            // For each place, get the icon, place name, and location.
+            markers = [];
+            var bounds = new google.maps.LatLngBounds();
+            for (var i = 0, place; place = places[i]; i++) {
+
+
+                bounds.extend(place.geometry.location);
+            }
+
+            map1.fitBounds(bounds);
+            map1.setZoom(11);
+
+
+        });
+
+        // Bias the SearchBox results towards places that are within the bounds of the
+        // current map's viewport.
+        google.maps.event.addListener(map1, 'bounds_changed', function () {
+            var bounds = map1.getBounds();
+            searchBox.setBounds(bounds);
+        });
+		
+		var allMarkers = [];
+		
+        $.each(app.view.dealers, function (k, v) {
+            if (!$.isEmptyObject(conf) && !$.isEmptyObject(conf.filter)) {
+                if ('salon' == conf.filter && $.isEmptyObject(v.salon_id)) {
+                    return;
+                }
+
+                if ('service' == conf.filter && $.isEmptyObject(v.service_id)) {
+                    return;
+                }
+
+                if ('pro' == conf.filter && $.isEmptyObject(v.dealers_pro)) {
+                    return;
+                }
+            }
+
+            var myLatlng1 = new google.maps.LatLng(v.gps_x, v.gps_y);
+            // Add markers
+            var marker1 = new google.maps.Marker({
+                position: myLatlng1,
+                map: map1,
+                icon: '/img/ico-marker3.png',
+                dealer: v,
+                scale: 4
+            });
+			
+			allMarkers.push(marker1);
+
+            google.maps.event.addListener(marker1, 'click', function () {
+                app.logger.var(marker1.dealer);
+
+                changeDealerInfo(marker1.dealer);
+				
+				for(var i=0; i<allMarkers.length; i++){
+					allMarkers[i].setIcon('/img/ico-marker3.png');
+				}
+				
+				marker1.setIcon('/img/ico-marker2.png');
+            });
+        })
+
+    }
 
 
     function loadTemplate(data) {
@@ -37,6 +169,8 @@
         app.container.append(html);
 
         app.view.afterWidget(widget);
+
+        mapInitialize(data);
     }
 })();
 
