a9660110d8f0dde58b960b4c10fd4c0ecad63911
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-08-12 11:27:05 +0300

    Sitemap.xml fix

--- frontend/config/_sitemap.php
+++ frontend/config/_sitemap.php
@@ -20,8 +20,8 @@ return [
                         // @var self $model
                         return
                             [
-                                'loc'        => \yii\helpers\Url::to('ru/article/' . $model->slug, true),
-                                'lastmod'    => strtotime($model->updated_at),
+                                'loc'        => \yii\helpers\Url::to('uk/article/' . $model->slug, true),
+                                'lastmod'    => $model->updated_at,
                                 'changefreq' => himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
                                 'priority'   => 0.8
                         ];
@@ -29,9 +29,8 @@ return [
                     ]
                 ]
             ],
-            //Promo
             [
-                'class'     => 'common\models\Promo',
+                'class'     => 'common\models\Article',
                 'behaviors' => [
                     'sitemap'     => [
                         'class' => himiklab\sitemap\behaviors\SitemapBehavior::className(),
@@ -44,8 +43,8 @@ return [
                             // @var self $model
                             return
                                 [
-                                    'loc'        => \yii\helpers\Url::to('ru/promo/' . $model->slug, true),
-                                    'lastmod'    => strtotime($model->updated_at),
+                                    'loc'        => \yii\helpers\Url::to('ru/article/' . $model->slug, true),
+                                    'lastmod'    => $model->updated_at,
                                     'changefreq' => himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
                                     'priority'   => 0.8
                             ];
@@ -53,9 +52,8 @@ return [
                         ]
                     ]
                 ],
-                //Page
                 [
-                    'class'     => 'common\models\Page',
+                    'class'     => 'common\models\Article',
                     'behaviors' => [
                         'sitemap'     => [
                             'class' => himiklab\sitemap\behaviors\SitemapBehavior::className(),
@@ -66,47 +64,184 @@ return [
                             },
                                 'dataClosure' => function ($model) {
                                 // @var self $model
-                                return [
-                                    'loc'        => \yii\helpers\Url::to('ru/' . $model->slug, true),
-                                    'lastmod'    => strtotime($model->updated_at),
-                                    'changefreq' => himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
-                                    'priority'   => 0.8
+                                return
+                                    [
+                                        'loc'        => \yii\helpers\Url::to('en/article/' . $model->slug, true),
+                                        'lastmod'    => $model->updated_at,
+                                        'changefreq' => himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
+                                        'priority'   => 0.8
                                 ];
                             }
-                            ],
-                        ],
+                            ]
+                        ]
                     ],
-                ],
-                'urls'        => [
-                // your additional urls
-                /* [
-                  'loc'        => '/news/index',
-                  'changefreq' => \himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
-                  'priority'   => 0.8,
-                  'news'       => [
-                  'publication'      => [
-                  'name'     => 'Example Blog',
-                  'language' => 'en',
-                  ],
-                  'access'           => 'Subscription',
-                  'genres'           => 'Blog, UserGenerated',
-                  'publication_date' => 'YYYY-MM-DDThh:mm:ssTZD',
-                  'title'            => 'Example Title',
-                  'keywords'         => 'example, keywords, comma-separated',
-                  'stock_tickers'    => 'NASDAQ:A, NASDAQ:B',
-                  ],
-                  'images'     => [
-                  [
-                  'loc'          => 'http://example.com/image.jpg',
-                  'caption'      => 'This is an example of a caption of an image',
-                  'geo_location' => 'City, State',
-                  'title'        => 'Example image',
-                  'license'      => 'http://example.com/license',
-                  ],
-                  ],
-                  ], */
-                ],
-                'enableGzip'  => false, // default is false
-                'cacheExpire' => 1, // 1 second. Default is 24 hours
-            ];
-            
+                    //Promo
+                    [
+                        'class'     => 'common\models\Promo',
+                        'behaviors' => [
+                            'sitemap'     => [
+                                'class' => himiklab\sitemap\behaviors\SitemapBehavior::className(),
+                                'scope' => function ($model) {
+                                    // @var \yii\db\ActiveQuery $model
+                                    $model->select(['slug', 'updated_at']);
+                                    $model->andWhere(['status' => 1]);
+                                },
+                                    'dataClosure' => function ($model) {
+                                    // @var self $model
+                                    return
+                                        [
+                                            'loc'        => \yii\helpers\Url::to('uk/promo/' . $model->slug, true),
+                                            'lastmod'    => $model->updated_at,
+                                            'changefreq' => himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
+                                            'priority'   => 0.8
+                                    ];
+                                }
+                                ]
+                            ]
+                        ],
+                        [
+                            'class'     => 'common\models\Promo',
+                            'behaviors' => [
+                                'sitemap'     => [
+                                    'class' => himiklab\sitemap\behaviors\SitemapBehavior::className(),
+                                    'scope' => function ($model) {
+                                        // @var \yii\db\ActiveQuery $model
+                                        $model->select(['slug', 'updated_at']);
+                                        $model->andWhere(['status' => 1]);
+                                    },
+                                        'dataClosure' => function ($model) {
+                                        // @var self $model
+                                        return
+                                            [
+                                                'loc'        => \yii\helpers\Url::to('ru/promo/' . $model->slug, true),
+                                                'lastmod'    => $model->updated_at,
+                                                'changefreq' => himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
+                                                'priority'   => 0.8
+                                        ];
+                                    }
+                                    ]
+                                ]
+                            ],
+                            [
+                                'class'     => 'common\models\Promo',
+                                'behaviors' => [
+                                    'sitemap'     => [
+                                        'class' => himiklab\sitemap\behaviors\SitemapBehavior::className(),
+                                        'scope' => function ($model) {
+                                            // @var \yii\db\ActiveQuery $model
+                                            $model->select(['slug', 'updated_at']);
+                                            $model->andWhere(['status' => 1]);
+                                        },
+                                            'dataClosure' => function ($model) {
+                                            // @var self $model
+                                            return
+                                                [
+                                                    'loc'        => \yii\helpers\Url::to('en/promo/' . $model->slug, true),
+                                                    'lastmod'    => $model->updated_at,
+                                                    'changefreq' => himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
+                                                    'priority'   => 0.8
+                                            ];
+                                        }
+                                        ]
+                                    ]
+                                ],
+                                //Page
+                                [
+                                    'class'     => 'common\models\Page',
+                                    'behaviors' => [
+                                        'sitemap'     => [
+                                            'class' => himiklab\sitemap\behaviors\SitemapBehavior::className(),
+                                            'scope' => function ($model) {
+                                                // @var \yii\db\ActiveQuery $model
+                                                $model->select(['slug', 'updated_at']);
+                                                $model->andWhere(['status' => 1]);
+                                            },
+                                                'dataClosure' => function ($model) {
+                                                // @var self $model
+                                                return [
+                                                    'loc'        => \yii\helpers\Url::to('uk/' . $model->slug, true),
+                                                    'lastmod'    => $model->updated_at,
+                                                    'changefreq' => himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
+                                                    'priority'   => 0.8
+                                                ];
+                                            }
+                                            ],
+                                        ],
+                                    ],
+                                    [
+                                        'class'     => 'common\models\Page',
+                                        'behaviors' => [
+                                            'sitemap'     => [
+                                                'class' => himiklab\sitemap\behaviors\SitemapBehavior::className(),
+                                                'scope' => function ($model) {
+                                                    // @var \yii\db\ActiveQuery $model
+                                                    $model->select(['slug', 'updated_at']);
+                                                    $model->andWhere(['status' => 1]);
+                                                },
+                                                    'dataClosure' => function ($model) {
+                                                    // @var self $model
+                                                    return [
+                                                        'loc'        => \yii\helpers\Url::to('ru/' . $model->slug, true),
+                                                        'lastmod'    => $model->updated_at,
+                                                        'changefreq' => himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
+                                                        'priority'   => 0.8
+                                                    ];
+                                                }
+                                                ],
+                                            ],
+                                        ],
+                                        [
+                                            'class'     => 'common\models\Page',
+                                            'behaviors' => [
+                                                'sitemap'     => [
+                                                    'class' => himiklab\sitemap\behaviors\SitemapBehavior::className(),
+                                                    'scope' => function ($model) {
+                                                        // @var \yii\db\ActiveQuery $model
+                                                        $model->select(['slug', 'updated_at']);
+                                                        $model->andWhere(['status' => 1]);
+                                                    },
+                                                        'dataClosure' => function ($model) {
+                                                        // @var self $model
+                                                        return [
+                                                            'loc'        => \yii\helpers\Url::to('en/' . $model->slug, true),
+                                                            'lastmod'    => $model->updated_at,
+                                                            'changefreq' => himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
+                                                            'priority'   => 0.8
+                                                        ];
+                                                    }
+                                                    ],
+                                                ],
+                                            ],
+                                        ],
+                                        'urls'        => [
+                                        // your additional urls
+                                        /* [
+                                          'loc'        => '/news/index',
+                                          'changefreq' => \himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
+                                          'priority'   => 0.8,
+                                          'news'       => [
+                                          'publication'      => [
+                                          'name'     => 'Example Blog',
+                                          'language' => 'en',
+                                          ],
+                                          'access'           => 'Subscription',
+                                          'genres'           => 'Blog, UserGenerated',
+                                          'publication_date' => 'YYYY-MM-DDThh:mm:ssTZD',
+                                          'title'            => 'Example Title',
+                                          'keywords'         => 'example, keywords, comma-separated',
+                                          'stock_tickers'    => 'NASDAQ:A, NASDAQ:B',
+                                          ],
+                                          'images'     => [
+                                          [
+                                          'loc'          => 'http://example.com/image.jpg',
+                                          'caption'      => 'This is an example of a caption of an image',
+                                          'geo_location' => 'City, State',
+                                          'title'        => 'Example image',
+                                          'license'      => 'http://example.com/license',
+                                          ],
+                                          ],
+                                          ], */
+                                        ],
+                                        'enableGzip'  => false, // default is false
+                                        'cacheExpire' => 1, // 1 second. Default is 24 hours
+                                    ];
