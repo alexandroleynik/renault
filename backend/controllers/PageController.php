8ab98f3529bf96f702935d34d99ea356f4faafa6
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-09-14 11:33:59 +0300

    fix

--- backend/controllers/PageController.php
+++ backend/controllers/PageController.php
@@ -100,13 +100,13 @@ class PageController extends Controller
         if ($model->load(Yii::$app->request->post()) && Page::multiSave($model)) {
             return $this->redirect(['index']);
         } else {
-            switch (Yii::$app->request->get('scenario')) {
+            /*switch (Yii::$app->request->get('scenario')) {
                 case 'extend' :
                     $viewName = 'extend';
                     break;
                 default :
                     $viewName = 'create';
-            }
+            }*/
 
             return $this->render($viewName, [
                     'model' => $model
