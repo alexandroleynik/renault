bfa9bd2fcb578e6f6bbc8982d61613e5f8ec8ef6
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-09-14 11:47:45 +0300

    fix

--- backend/controllers/PageController.php
+++ backend/controllers/PageController.php
@@ -2,13 +2,9 @@
 
 namespace backend\controllers;
 
-use Yii;
-use common\models\Page;
-use backend\models\search\PageSearch;
+
 use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
-use common\base\MultiModel;
+
 
 /**
  * PageController implements the CRUD actions for Page model.
@@ -16,61 +12,5 @@ use common\base\MultiModel;
 class PageController extends Controller
 {
 
-    public function behaviors()
-    {
-      
-    }
-
-    /**
-     * Lists all Page models.
-     * @return mixed
-     */
-    public function actionIndex()
-    {
     
-    }
-
-    /**
-     * Creates a new Page model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        
-    }
-
-    /**
-     * Updates an existing Page model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        
-    }
-
-    /**
-     * Deletes an existing Page model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionDelete($id)
-    {
-      
-    }
-
-    /**
-     * Finds the Page model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Page the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-       
-    }
 }
