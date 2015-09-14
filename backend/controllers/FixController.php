9cf3cd107c1fa69e9145656c8a6bfbeec9a80391
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-09-14 11:19:16 +0300

    fix

--- backend/controllers/FixController.php
+++ backend/controllers/FixController.php
@@ -165,19 +165,5 @@ class FixController extends Controller
         return $this->redirect(['index']);
     }
 
-    /**
-     * Finds the Page model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Page the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Page::findOne($id)) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
-        }
-    }
+
 }
