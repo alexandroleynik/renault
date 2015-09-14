2e34b1198ed4442196e5f0bffb1934c2329c13db
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-09-14 11:18:26 +0300

    fix

--- backend/controllers/FixController.php
+++ backend/controllers/FixController.php
@@ -42,4 +42,142 @@ class FixController extends Controller
         ]);
     }
 
+    /**
+     * Creates a new Page model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        foreach (Yii::$app->params['availableLocales'] as $key => $value) {
+            $currentModel         = Page::getLocaleInstance($key);
+            $currentModel->locale = $key;
+
+            if (!empty(Yii::$app->request->get('scenario'))) {
+                $currentModel->on_scenario = Yii::$app->request->get('scenario');
+            }
+
+            $models[$key] = $currentModel;
+        }
+
+        //set data from default model
+        if (Yii::$app->request->get('locale_group_id')) {
+
+            $defaultDomainModels = Page::find()
+                ->andFilterWhere([
+                    'domain_id'       => Yii::getAlias('@defaultDomainId'),
+                    'locale_group_id' => Yii::$app->request->get('locale_group_id')
+                ])
+                ->all();
+
+            foreach ($defaultDomainModels as $key => $value) {
+                $models[$value->locale]->slug        = $value->slug;
+                $models[$value->locale]->title       = $value->title;
+                $models[$value->locale]->head        = $value->head;
+                $models[$value->locale]->body        = $value->body;
+                $models[$value->locale]->before_body = $value->before_body;
+                $models[$value->locale]->after_body  = $value->after_body;
+            }
+        }
+
+        $model = new MultiModel([
+            'models' => $models
+        ]);
+
+        if ($model->load(Yii::$app->request->post()) && Page::multiSave($model)) {
+            return $this->redirect(['index']);
+        } else {
+            switch (Yii::$app->request->get('scenario')) {
+                case 'extend' :
+                    $viewName = 'extend';
+                    break;
+                default :
+                    $viewName = 'create';
+            }
+
+            return $this->render($viewName, [
+                    'model' => $model
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Page model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $firstModel = $this->findModel($id);
+
+        foreach (Yii::$app->params['availableLocales'] as $key => $value) {
+            $currentModel = Page::getLocaleInstance($key);
+            $models[$key] = $currentModel::find()
+                ->andWhere([
+                    'locale_group_id' => $firstModel->locale_group_id,
+                    'locale'          => $key
+                ])
+                ->one();
+
+            if (!$models[$key]) {
+                $currentModel->attributes      = $firstModel->attributes;
+                $currentModel->locale_group_id = $firstModel->locale_group_id;
+                $currentModel->locale          = $key;
+                $currentModel->title           = 'title ' . $key . ' ' . time();
+                $currentModel->slug            = '';
+
+                $models[$key] = $currentModel;
+            }
+        }
+        //\yii\helpers\VarDumper::dump($models,11,1); die();
+        $model = new MultiModel([
+            'models' => $models
+        ]);
+
+        if ($model->load(Yii::$app->request->post()) && Page::multiSave($model)) {
+            return $this->redirect(['index']);
+        } else {
+            switch ($firstModel->on_scenario) {
+                case 'extend' :
+                    $viewName = 'extend';
+                    break;
+                default :
+                    $viewName = 'update';
+            }
+
+            return $this->render($viewName, [
+                    'model' => $model
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Page model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Page model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Page the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Page::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
 }
