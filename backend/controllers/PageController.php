f191c72b5d0f0d8131a7382771ab25e7f6f69229
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-09-14 11:42:55 +0300

    fix

--- backend/controllers/PageController.php
+++ backend/controllers/PageController.php
@@ -66,7 +66,7 @@ class PageController extends Controller
             $currentModel         = Page::getLocaleInstance($key);
             $currentModel->locale = $key;
 
-            if (!empty(Yii::$app->request->get('scenario'))) {
+            if (Yii::$app->request->get('scenario')) {
                 $currentModel->on_scenario = Yii::$app->request->get('scenario');
             }
 
