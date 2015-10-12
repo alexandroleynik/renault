c06b5b7fe8c0b66d4a6c491a1f1fa9e185f0605c
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-10-12 14:54:28 +0300

    user domain_id fix

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -70,22 +70,16 @@ class UserController extends Controller
             return $this->redirect(['index']);
         }
 
-        /*$m = array_merge(['0' => ''], ArrayHelper::map(
-                        Domain::find()->active()->all(), 'id', 'title'
-                ));*/
+        /* $m = array_merge(['0' => ''], ArrayHelper::map(
+          Domain::find()->active()->all(), 'id', 'title'
+          )); */
 
 
-        $m =  ArrayHelper::map( Domain::find()->active()->all(), 'id', 'title');
-        $m['0'] = '';
-        ksort($m);
-        \yii\helpers\VarDumper::dump($m, 11, 1); die();
-        
+
         return $this->render('create', [
                 'model'   => $model,
                 'roles'   => User::getCustomRoles(),
-                'domains' => array_merge(['0' => ''], ArrayHelper::map(
-                        Domain::find()->active()->all(), 'id', 'title'
-                ))
+                'domains' => $this->_getDomainsArray()
         ]);
     }
 
@@ -105,9 +99,7 @@ class UserController extends Controller
         return $this->render('update', [
                 'model'   => $model,
                 'roles'   => User::getCustomRoles(),
-                'domains' => array_merge(['0' => ''], ArrayHelper::map(
-                    Domain::find()->active()->all(), 'id', 'title'
-                ))
+                'domains' => $this->_getDomainsArray()
         ]);
     }
 
@@ -139,5 +131,14 @@ class UserController extends Controller
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
         }
+    }
+
+    private function _getDomainsArray()
+    {
+        $domainsArray      = ArrayHelper::map(Domain::find()->active()->all(), 'id', 'title');
+        $domainsArray['0'] = '';
+        ksort($domainsArray);
+
+        return $domainsArray;
     }
 }
