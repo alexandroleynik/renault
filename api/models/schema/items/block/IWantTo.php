b86ea3828ae67920a6d07f47fe1868b5965b9b37
Viktor Afanasjev <afanasjev-v@yandex.ru>
2015-10-15 17:35:42 +0300

    Update IWantTo.php

--- api/models/schema/items/block/IWantTo.php
+++ api/models/schema/items/block/IWantTo.php
@@ -13,7 +13,7 @@ use \Yii;
 
 class IWantTo extends Base
 {
-    protected $wid    = 'i-want-to';
+    protected $wid = 'i-want-to';
     protected $wtitle = 'I want to';
 
     public function __construct()
@@ -29,47 +29,73 @@ class IWantTo extends Base
             'title' => Yii::t('backend', 'I want to text'),
             'default' => 'Я хотiв би',
         ];
-        $this->data['properties']['book_a_test_drive_text']     = [
-            'type'    => 'string',
-            'title' => Yii::t('backend', 'Book a test drive text'),
-            'default' => 'записатися<br> на тест-драйв',
-        ];
-        $this->data['properties']['twoPartUrlToBookATestDrive'] = [
-            'type'    => 'string',
-            'title' => Yii::t('backend', 'link Book a test drive text'),
-            'default' => '/page/view/book-a-test-drive',
-        ];
-        $this->data['properties']['load_booking_text']          = [
-            'type'    => 'string',
-            'title' => Yii::t('backend', 'Load booking text'),
-            'default' => 'завантажити<br> брошуру',
-        ];
-        $this->data['properties']['twoPartUrlToLoadBooking']    = [
-            'type'    => 'string',
-            'title' => Yii::t('backend', 'link Load booking text'),
-            'default' => 'http://servicebooking.renault.co.uk',
-        ];
-        $this->data['properties']['load_price_list']            = [
-            'type'    => 'string',
-            'title' => Yii::t('backend', 'Load price list'),
-            'default' => 'завантажити<br> прайс-лист',
-        ];
-        $this->data['properties']['twoPartUrlToBrochures']      = [
-            'type'    => 'string',
-            'title' => Yii::t('backend', 'link Load price list'),
-            'default' => '/page/view/brochures',
-        ];
-        $this->data['properties']['contact_with_dealer_text']   = [
-            'type'    => 'string',
-            'title' => Yii::t('backend', 'find a dealer'),
-            'default' => 'звязатися з<br>диллером',
-        ];
-        $this->data['properties']['twoPartUrlToFindADealer']    = [
-            'type'    => 'string',
-            'title' => Yii::t('backend', 'link to find a dealer'),
-            'default' => '/page/view/contact-form',
-        ];
+        $this->data['properties']["buttons"] = [
+            "type" => "array",
+            "title" => Yii::t('backend', "Buttons"),
+            "maxItems" => '4',
+            "items" => [
+                "type" => "object",
+                "title" => Yii::t('backend', "button"),
+                "format" => 'grid',
+                "properties" => [
+                    "image" => [
+                        "type" => "string",
+                        "format" => "url",
+                        "title" => Yii::t('backend', 'image'),
+                        "propertyOrder"=> 1,
+                        "options" => [
+                            "upload" => true,
+                            'grid_columns' => 12,
+                            "input_width" => "300px"
+                        ],
+                        "links" => [
+                            [
+                                "href" => "{{self}}",
+                                "rel" => "View file"
+                            ]
+                        ]
+                    ],
+                    "name" => [
+                        "type" => "string",
+                        'title' => Yii::t('backend', 'name'),
+                        "default" => "name",
+                        "propertyOrder"=> 10,
+                        'options' => [
+                            'grid_columns' => 4
+                        ]
 
+                    ],
+                    "host" => [
+                        'type'          => 'string',
+                        'propertyOrder' => 20,
+
+                        'title' => Yii::t('backend', 'host'),
+                        'enum'          => [
+                            '0' => '',
+                            '1' => '@frontend',
+                        ],
+                        'options'       => [
+                            'grid_columns' => 4,
+                            'enum_titles' => [
+                                '0' => 'Зовнішній сайт',
+                                '1' => '@frontend',
+
+                            ],
+                        ],
+                        'default'       => '@frontend',
+                    ],
+                    "url" => [
+                        'title' => Yii::t('backend', 'url'),
+                        "type" => "string",
+                        "propertyOrder"=> 30,
+                        'options' => [
+                            'grid_columns' => 4
+                        ]
+                    ],
+                ]
+            ],
+
+        ];
 
         return $this->data;
     }
