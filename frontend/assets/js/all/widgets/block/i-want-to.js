152ad05cd16aceac099662095b04ee09d900589d
Viktor Afanasjev <afanasjev-v@yandex.ru>
2015-10-15 18:03:03 +0300

    Update i-want-to.js

--- frontend/assets/js/all/widgets/block/i-want-to.js
+++ frontend/assets/js/all/widgets/block/i-want-to.js
@@ -20,11 +20,12 @@ app.view.wfn['i-want-to'] = (function () {
         var data = widget;  
         data.urlSite = app.view.helper.preffix;
         data.urlToLoadBooking = '';
-$.each(data.items, function (key, val) {
-            if ('@frontend' == val.host) {
-                data.items[key].viewUrl = app.view.helper.preffix + val.url;
-            }
-        });
+        
+        // $.each(data.items, function (key, val) {
+        //     if ('@frontend' == val.host) {
+        //         data.items[key].viewUrl = app.view.helper.preffix + val.url;
+        //     }
+        // });
        // data.urlToBrochures = app.view.helper.preffix;
        //data.urlToFindADealer = app.view.helper.preffix + '/contact-form';
         
