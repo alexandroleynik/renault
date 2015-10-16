e16a4465bdfa8fbd91aeb185d5a3cb3909aa84c3
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-10-16 11:12:23 +0300

    Раздел Коммерческие автомобили

--- backend/views/model/_form.php
+++ backend/views/model/_form.php
@@ -1,23 +1,27 @@
 <?php
 
-/*use common\models\Model;
+use \yii\helpers\ArrayHelper;
 
-Model::getLeftMenuListItems2();
-
-die();*/
-?>
-
-<?php
 $mId = strtolower($model::getClassNameNoNamespace());
+
+$categoryList = ArrayHelper::map($categories, 'id', 'title');
+foreach ($categoryList as $key => $value) {
+    $categoryList[$key] = \Yii::t('backend', $value);
+}
+
 ?>
 
 <?php echo $form->field($model, 'title')->textInput(['maxlength' => 512]) ?>
 <?php echo $form->field($model, 'price')->textInput(['maxlength' => 512]) ?>
 
 <?php
-echo $form->field($model, 'categoriesList')->dropDownList(\yii\helpers\ArrayHelper::map(
-        $categories, 'id', 'title'
-    ), ['prompt' => '', 'multiple' => true, 'style' => 'border: none; margin:0px; padding:0px;']);
+echo $form->field($model, 'categoriesList')->dropDownList(
+    $categoryList, [
+    'prompt'   => '',
+    'multiple' => true,
+    'style'    => 'border: none; margin:0px; padding:0px;'
+    ]
+);
 
 $js = '$("#' . $mId . '-categorieslist").select2();';
 $this->registerJs($js);
