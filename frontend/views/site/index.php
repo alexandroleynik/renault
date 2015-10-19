94c6b20a3d712f2ec7ee46fb31938a816bd91b55
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-10-19 09:03:05 +0300

    test proxy

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -28,7 +28,7 @@ $js = 'app.config = ' . json_encode(Yii::$app->keyStorage->getAllArray()) . ';'
     . 'app.config.frontend_app_files_midified = ' . json_encode(FileLogic::getModifiedTime(FileHelper::findFiles(Yii::getAlias('@webroot/templates')))) . ';'
     . 'app.config.frontend_app_api_url = "' . Yii::getAlias('@apiUrl') . '";';
 
-$this->registerJs($js, \yii\web\View::POS_END);
+//$this->registerJs($js, \yii\web\View::POS_END);
 
 //yii\helpers\VarDumper::dump(common\models\Page::getMetaTags(), 11, 1); die();
 
