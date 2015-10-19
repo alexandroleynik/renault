50649b22ba02aa4eea5aa569e6b8bd63c5716bcc
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-10-19 11:00:13 +0300

    test proxy

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -14,21 +14,21 @@ Page::switchToUrlLocale();
 <?php require_once Yii::getAlias('@webroot/templates/app.html') ?>
 
 <?php
-$js = 'app.config = ' . json_encode(Yii::$app->keyStorage->getAllArray()) . ';'
-    . 'app.config.frontend_app_debug = "' . YII_DEBUG . '";'
-    . 'app.config.frontend_app_web_url = "' . Yii::getAlias('@web') . '";'
-    . 'app.config.frontend_app_frontend_url = "' . Yii::getAlias('@frontendUrl') . '";'
-    . 'app.config.frontend_app_domain_id = "' . Yii::getAlias('@domainId') . '";'
-    . 'app.config.frontend_app_default_domain_id = "' . Yii::getAlias('@defaultDomainId') . '";'
-    . 'app.config.frontend_app_dealer_id = "' . Yii::getAlias('@dealerId') . '";'
-    . 'app.config.frontend_app_locale = "' . Yii::$app->language . '";'
-    . 'app.config.frontend_app_facebook_app_id = "' . getenv('FACEBOOK_APP_ID') . '";'
-    . 'app.config.frontend_app_instagram_client_id = "' . getenv('INSTAGRAM_CLIENT_ID') . '";'
-    . 'app.config.frontend_app_code_body_end = "' . htmlspecialchars(str_replace(array("\r\n", "\r", "\n"), "", DbText::widget(['key' => 'frontend.code.body.end', 'domain_id' => Yii::getAlias('@domainId')]))) . '";'
-    . 'app.config.frontend_app_files_midified = ' . json_encode(FileLogic::getModifiedTime(FileHelper::findFiles(Yii::getAlias('@webroot/templates')))) . ';'
-    . 'app.config.frontend_app_api_url = "' . Yii::getAlias('@apiUrl') . '";';
+$js = 'server_config = ' . json_encode(Yii::$app->keyStorage->getAllArray()) . ';'
+    . 'server_config.frontend_app_debug = "' . YII_DEBUG . '";'
+    . 'server_config.frontend_app_web_url = "' . Yii::getAlias('@web') . '";'
+    . 'server_config.frontend_app_frontend_url = "' . Yii::getAlias('@frontendUrl') . '";'
+    . 'server_config.frontend_app_domain_id = "' . Yii::getAlias('@domainId') . '";'
+    . 'server_config.frontend_app_default_domain_id = "' . Yii::getAlias('@defaultDomainId') . '";'
+    . 'server_config.frontend_app_dealer_id = "' . Yii::getAlias('@dealerId') . '";'
+    . 'server_config.frontend_app_locale = "' . Yii::$app->language . '";'
+    . 'server_config.frontend_app_facebook_app_id = "' . getenv('FACEBOOK_APP_ID') . '";'
+    . 'server_config.frontend_app_instagram_client_id = "' . getenv('INSTAGRAM_CLIENT_ID') . '";'
+    . 'server_config.frontend_app_code_body_end = "' . htmlspecialchars(str_replace(array("\r\n", "\r", "\n"), "", DbText::widget(['key' => 'frontend.code.body.end', 'domain_id' => Yii::getAlias('@domainId')]))) . '";'
+    . 'server_config.frontend_app_files_midified = ' . json_encode(FileLogic::getModifiedTime(FileHelper::findFiles(Yii::getAlias('@webroot/templates')))) . ';'
+    . 'server_config.frontend_app_api_url = "' . Yii::getAlias('@apiUrl') . '";';
 
-$this->registerJs($js, \yii\web\View::POS_END);
+$this->registerJs($js, \yii\web\View::POS_HEAD);
 
 //yii\helpers\VarDumper::dump(common\models\Page::getMetaTags(), 11, 1); die();
 
