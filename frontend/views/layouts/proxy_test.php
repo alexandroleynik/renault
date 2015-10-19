d6fd4c8aec370143da13a441c22496f53b7dcfa4
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-10-19 09:38:09 +0300

    test proxy

--- frontend/views/layouts/proxy_test.php
+++ frontend/views/layouts/proxy_test.php
@@ -57,13 +57,14 @@ use common\widgets\DbText;
 
     <main id="container" role="main">
         <?php echo $content ?>
+        TEST
     </main>
     
-    <?php require_once '_loader.php'; ?>
+    
 
     <?= Html::jsFile(YII_DEBUG ? '@web/js/lib.js?v=' . filemtime(Yii::getAlias('@webroot/js/lib.min.js')) : '@web/js/lib.min.js?v=' . filemtime(Yii::getAlias('@webroot/js/lib.min.js'))) ?>
-    <?= Html::jsFile(YII_DEBUG ? '@web/js/all.js?v=' . filemtime(Yii::getAlias('@webroot/js/all.min.js')) : '@web/js/all.min.js?v=' . filemtime(Yii::getAlias('@webroot/js/all.min.js'))) ?>
     
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&language=uk"></script>
     <?php $this->endBody() ?>
 </body>
 </html>
