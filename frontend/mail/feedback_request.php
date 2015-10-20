c17005dcdf85d0d4342d2a0312a3e142dc41af9e
aivavic <afanasjev-v@yandex.ru>
2015-10-20 11:12:47 +0300

    fix corporate-sales

--- frontend/mail/feedback_request.php
+++ frontend/mail/feedback_request.php
@@ -4,6 +4,6 @@
 <?php echo Yii::t('api', 'secondname') ?>: <?php echo $secondname ?><br>
 <?php echo Yii::t('api', 'lastname') ?>: <?php echo $lastname ?>
 <?php echo Yii::t('api', 'email') ?>: <?php echo $email ?><br>
-<?php echo Yii::t('api', '$post') ?>: <?php echo $phone ?><br>
+<?php echo Yii::t('api', 'phone') ?>: <?php echo $phone ?><br>
 <?php echo Yii::t('api', 'Message') ?>: <?php echo $message ?>
 
