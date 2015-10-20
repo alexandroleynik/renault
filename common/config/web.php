e3ad53a4f2f6ba200e791d87adae72ba86294619
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-10-20 08:32:41 +0300

    test proxy

--- common/config/web.php
+++ common/config/web.php
@@ -1,24 +1,38 @@
 <?php
 $config = [
-    'components' => [
+    'components'    => [
         'assetManager' => [
             'class'           => 'yii\web\AssetManager',
             'linkAssets'      => true,
-            'appendTimestamp' => YII_ENV_DEV            
+            'appendTimestamp' => YII_ENV_DEV
         ]
     ],
-    'as locale'  => [
+    'as locale'     => [
         'class'                   => 'common\behaviors\LocaleBehavior',
         'enablePreferredLanguage' => true
-    ]
+    ],
+    'as corsFilter' => [
+        'class' => \yii\filters\Cors::className(),
+        'cors'  => [
+            // allow access to
+            'Origin' => array_merge(
+                [
+                'https://fr.proxfree.com',
+                'http://fr.proxfree.com',
+                Yii::getAlias('@frontendUrl'),
+                Yii::getAlias('@backendUrl')
+                ], explode(',', Yii::getAlias('@frontendUrls'))
+            ),
+        ],
+    ],
 ];
 
 if (YII_DEBUG) {
-    /*$config['bootstrap'][]      = 'debug';
-    $config['modules']['debug'] = [
-        'class'      => 'yii\debug\Module',
-        'allowedIPs' => ['127.0.0.1', '::1', '192.168.33.1'],
-    ];*/
+    /* $config['bootstrap'][]      = 'debug';
+      $config['modules']['debug'] = [
+      'class'      => 'yii\debug\Module',
+      'allowedIPs' => ['127.0.0.1', '::1', '192.168.33.1'],
+      ]; */
 }
 
 if (YII_ENV_DEV) {
