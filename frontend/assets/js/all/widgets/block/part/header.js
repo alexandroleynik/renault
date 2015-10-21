11978cb0655f0aca4c5e1db4be43a76b1fe12016
Viktor Afanasjev <afanasjev-v@yandex.ru>
2015-10-21 11:09:34 +0300

    Update header.js

--- frontend/assets/js/all/widgets/block/part/header.js
+++ frontend/assets/js/all/widgets/block/part/header.js
@@ -127,14 +127,15 @@ app.view.wfn['header'] = (function () {
                             if ('@frontend' == val.host) {
                                 data.menu[key].host = app.view.helper.preffix;
                             }
-                             if (data.menu[key].submenu) {
-                            $.each(this.submenu, function(subkey, subval){
-                            
-                                if ('@frontend' == subval.host) {
-                                    data.menu[key].submenu[subkey].host = app.view.helper.preffix;
-                                }
-                            })
-                             }
+                           if (data.menu[key].submenu) {
+
+
+                        $.each(data.menu[key].submenu, function (subkey, subval) {
+                            if('@frontend' == subval.host)     {
+                                       data.menu[key].submenu[subkey].host = app.view.helper.preffix;
+                            }
+                        });
+                    }
 
                         });
 
