a327eca23c62dfcd2c30b3b619535f87576cbc45
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-11-03 11:37:58 +0200

    info-menu blacklist filter

--- frontend/assets/js/all/widgets/arrays/tables/info-menu.js
+++ frontend/assets/js/all/widgets/arrays/tables/info-menu.js
@@ -1,7 +1,7 @@
 app.view.wfn['info-menu'] = (function () {
     /*** process   ***/
     //run()->loadData()->loadTemplate(data)->renderWidget(html);
-    
+
     var widget = app.view.getCurrentWidget();
     var template = '/templates/arrays/tables/info-menu.html';
 
@@ -24,15 +24,19 @@ app.view.wfn['info-menu'] = (function () {
             }
         });
 
+        data.items = data.items.filter(function (v) {
+            return app.view.isDealerBlackListPage('/' + app.router.locale + v.url) ? false : true;
+        });
+
         loadTemplate(data);
     }
 
 
     function loadTemplate(data) {
         app.logger.func('loadTemplate(data)');
-        
+
         var v = app.config.frontend_app_files_midified[template];
-        
+
         app.templateLoader.getTemplateAjax(app.config.frontend_app_web_url + template + '?v=' + v, function (template) {
             renderWidget(template(data));
         });
--- frontend/web/js/all.js
+++ frontend/web/js/all.js
@@ -4058,7 +4058,7 @@ app.view.wfn['dealer-quest-box'] = (function () {
 app.view.wfn['info-menu'] = (function () {
     /*** process   ***/
     //run()->loadData()->loadTemplate(data)->renderWidget(html);
-    
+
     var widget = app.view.getCurrentWidget();
     var template = '/templates/arrays/tables/info-menu.html';
 
@@ -4081,15 +4081,19 @@ app.view.wfn['info-menu'] = (function () {
             }
         });
 
+        data.items = data.items.filter(function (v) {
+            return app.view.isDealerBlackListPage('/' + app.router.locale + v.url) ? false : true;
+        });
+
         loadTemplate(data);
     }
 
 
     function loadTemplate(data) {
         app.logger.func('loadTemplate(data)');
-        
+
         var v = app.config.frontend_app_files_midified[template];
-        
+
         app.templateLoader.getTemplateAjax(app.config.frontend_app_web_url + template + '?v=' + v, function (template) {
             renderWidget(template(data));
         });
