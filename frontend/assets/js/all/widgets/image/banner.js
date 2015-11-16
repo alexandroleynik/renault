1db4912d8654541d231599e6a35213693ce3dfa0
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-11-16 15:30:33 +0200

    banner fix

--- frontend/assets/js/all/widgets/image/banner.js
+++ frontend/assets/js/all/widgets/image/banner.js
@@ -1,7 +1,7 @@
 app.view.wfn['banner'] = (function () {
     /*** process   ***/
     //run()->loadData()->loadTemplate(data)->renderWidget(html);
-    
+
     var widget = app.view.getCurrentWidget();
     var template = '/templates/image/banner.html';
 
@@ -14,7 +14,7 @@ app.view.wfn['banner'] = (function () {
 
     function loadData() {
         app.logger.func('loadData()');
-        
+
         var data = widget;
 
         app.view.href = data.href;
@@ -27,9 +27,9 @@ app.view.wfn['banner'] = (function () {
 
     function loadTemplate(data) {
         app.logger.func('loadTemplate(data)');
-        
+
         var v = app.config.frontend_app_files_midified[template];
-        
+
         app.templateLoader.getTemplateAjax(app.config.frontend_app_web_url + template + '?v=' + v, function (template) {
             renderWidget(template(data));
         });
@@ -38,20 +38,22 @@ app.view.wfn['banner'] = (function () {
     function renderWidget(html) {
         app.logger.func('renderWidget(html)');
         $('#widget-wrapper-' + widget.uniqueKey).append(html);
-        $('.imgfs > img').click(function(){
+        $('.imgfs > img').click(function () {
             bannerClick();
         });
         app.view.afterWidget(widget);
     }
 
-    function bannerClick(){
-        
-        _gaq.push([
-        'trackEvent',
-            app.view.categorie,
-            app.view.subcategorie
-    ]);
-    window.open(app.view.href);
+    function bannerClick() {
+        if (_gaq) {
+            _gaq.push([
+                'trackEvent',
+                app.view.categorie,
+                app.view.subcategorie
+            ]);
+        }
+
+        window.open(app.view.href);
         return false;
     }
 });
