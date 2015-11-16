f96f3c4ab3b7201e957a279c5c648ed24183594e
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-11-16 15:27:38 +0200

    widget-text logic

--- backend/views/widget-text/index.php
+++ backend/views/widget-text/index.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            //['class' => 'yii\grid\SerialColumn'],
 
             'id',
             'key',
@@ -37,6 +37,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     Yii::t('backend', 'Enabled')
                 ],
             ],
+            'domain_id',
 
             [
                 'class' => 'yii\grid\ActionColumn',
