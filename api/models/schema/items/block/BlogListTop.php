24dda795ac8e8233a9f4ace65c07c724736c246c
Viktor Afanasjev <afanasjev-v@yandex.ru>
2015-11-17 21:19:21 +0200

    Update BlogListTop.php

--- api/models/schema/items/block/BlogListTop.php
+++ api/models/schema/items/block/BlogListTop.php
@@ -29,7 +29,7 @@ class BlogListTop extends Base
 
             "type"    => "string",
             "format"  => "url",
-            "title"   => Yii::t('backend', 'simple photo'),
+            "title"   => Yii::t('backend', 'image570х320'),
             "options" => [
                 "upload" => true
             ],
