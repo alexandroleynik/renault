da37b660c055448f0aeca8d731358eb8e54fab84
text <text@192.168.0.103>
2015-12-13 17:09:01 +0200

    fix www validation problems

--- frontend/views/layouts/_loader.php
+++ frontend/views/layouts/_loader.php
@@ -1,36 +1,4 @@
 
-<style>
-.preload-mask {
-display: block;
-position: fixed;
-width: 100%;
-width: 100vw;
-height: 100%;
-height: 100vh;
-top: 0;
-left: 0;
-background: white;
-content: "";
-    z-index: 99999;
-}
-
-.preload-logo {
-position: absolute;
-top: 0;
-left: 0;
-bottom: 0;
-right: 0;
-margin: auto;
-width: 300px;
-height: 130px;
-    z-index: 99999;
-}
-
-.preload-logo div {
-width: 160px;
-margin: 10px auto 0;
-}
-</style>
 <div class="preload-mask">
     <div class="preload-logo">
         <img src="/img/renault_main_logo.png" />
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -50,110 +50,139 @@ use common\widgets\DbText;
     <?= Html::cssFile(YII_DEBUG ? '@web/css/all.css?v=' . filemtime(Yii::getAlias('@webroot/css/all.min.css')) : '@web/css/all.min.css?v=' . filemtime(Yii::getAlias('@webroot/css/all.min.css'))) ?>
     <?= Html::cssFile('@web/plugins/rs-plugin/css/settings.css?v=' . filemtime(Yii::getAlias('@webroot/plugins/rs-plugin/css/settings.css'))) ?>
     <?php echo DbText::widget(['key' => 'frontend.code.head.end', 'domain_id' => Yii::getAlias('@domainId')]); ?>
-<style>
-    .disabled-page {
-        position: relative;
-        width: 100%;
-        height: 100vh;
-        min-height: 700px;
-        z-index: 100001;
-        background: white;
-    }
-        
-    .disabled-content {
-        width: 580px;
-        height: 620px;
-        text-align: center;
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        margin: auto;
-        z-index: 100002
-    }
-        
-    .disabled-content img {
-        margin: 0 auto 50px;
-    }
-        
-    .disabled-content h2 {
-        font-size: 48px;
-        line-height: 48px;
-        margin-bottom: 18px;
-    }
-        
-    .disabled-content p {
-        font-size: 22px;
-        line-height: 30px;
-    }
-        
-    .disabled-content p a {
-        text-decoration: underline;
-        color: #ffcf00;
-    }
-        
-    .disabled-content p a span {
-        color: #333;
-    }
-            
-    @media screen and (max-width: 1919px) {
-        .disabled-content img {
-            width: 210px;
-            margin: 0 auto 53px;
-        }
-            
-        .disabled-content h2 {
-            font-size: 36px;
-            line-height: 36px;
-            margin-bottom: 18px;
+    <style>
+        .disabled-page {
+            position: relative;
+            width: 100%;
+            height: 100vh;
+            min-height: 700px;
+            z-index: 100001;
+            background: white;
         }
 
-        .disabled-content p {
-            font-size: 18px;
-            line-height: 24px;
-            margin-bottom: 20px;
-        }
-    }
-        
-    @media screen and (max-width: 1023px) {
-        .disabled-content img {
-            width: 200px;
-            margin: 0 auto 60px;
-        }
-
-        .disabled-content p {
-            font-size: 17px;
-            line-height: 20px;
-            margin-bottom: 20px;
-        }
-    }
-        
-    @media screen and (max-width: 767px) {
         .disabled-content {
-            max-width: 80%;
-            height: 420px;
+            width: 580px;
+            height: 620px;
+            text-align: center;
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            margin: auto;
+            z-index: 100002
         }
-            
+
         .disabled-content img {
-            width: 144px;
             margin: 0 auto 50px;
         }
-            
+
         .disabled-content h2 {
-            font-size: 22px;
-            line-height: 22px;
+            font-size: 48px;
+            line-height: 48px;
             margin-bottom: 18px;
         }
 
         .disabled-content p {
-            font-size: 11px;
-            line-height: 14px;
-            margin-bottom: 20px;
+            font-size: 22px;
+            line-height: 30px;
         }
-    }
-</style>
-    
+
+        .disabled-content p a {
+            text-decoration: underline;
+            color: #ffcf00;
+        }
+
+        .disabled-content p a span {
+            color: #333;
+        }
+
+        @media screen and (max-width: 1919px) {
+            .disabled-content img {
+                width: 210px;
+                margin: 0 auto 53px;
+            }
+
+            .disabled-content h2 {
+                font-size: 36px;
+                line-height: 36px;
+                margin-bottom: 18px;
+            }
+
+            .disabled-content p {
+                font-size: 18px;
+                line-height: 24px;
+                margin-bottom: 20px;
+            }
+        }
+
+        @media screen and (max-width: 1023px) {
+            .disabled-content img {
+                width: 200px;
+                margin: 0 auto 60px;
+            }
+
+            .disabled-content p {
+                font-size: 17px;
+                line-height: 20px;
+                margin-bottom: 20px;
+            }
+        }
+
+        @media screen and (max-width: 767px) {
+            .disabled-content {
+                max-width: 80%;
+                height: 420px;
+            }
+
+            .disabled-content img {
+                width: 144px;
+                margin: 0 auto 50px;
+            }
+
+            .disabled-content h2 {
+                font-size: 22px;
+                line-height: 22px;
+                margin-bottom: 18px;
+            }
+
+            .disabled-content p {
+                font-size: 11px;
+                line-height: 14px;
+                margin-bottom: 20px;
+            }
+        }
+        .preload-mask {
+            display: block;
+            position: fixed;
+            width: 100%;
+            width: 100vw;
+            height: 100%;
+            height: 100vh;
+            top: 0;
+            left: 0;
+            background: white;
+            content: "";
+            z-index: 99999;
+        }
+
+        .preload-logo {
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            margin: auto;
+            width: 300px;
+            height: 130px;
+            z-index: 99999;
+        }
+
+        .preload-logo div {
+            width: 160px;
+            margin: 10px auto 0;
+        }
+    </style>    
 </head>
 <body>
     <?php $this->beginBody() ?>
