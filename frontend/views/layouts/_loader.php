1766164095737a615db631bfb2b8b220b538ebb7
text <text@192.168.0.103>
2015-12-13 17:10:30 +0200

    fix www validation problems

--- frontend/views/layouts/_loader.php
+++ frontend/views/layouts/_loader.php
@@ -1,7 +1,7 @@
 
 <div class="preload-mask">
     <div class="preload-logo">
-        <img src="/img/renault_main_logo.png" />
+        <img src="/img/renault_main_logo.png" alt=""/>
         <div id="loaderImage"></div>
     </div>
 </div>
