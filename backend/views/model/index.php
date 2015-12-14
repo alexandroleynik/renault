bc565d6607c7e48e2568c02124de4a9d8c47aa85
aivavic <afanasjev-v@yandex.ru>
2015-12-14 18:05:35 +0200

    fix

--- backend/views/model/index.php
+++ backend/views/model/index.php
@@ -95,7 +95,8 @@ $this->registerJs($js);
         'dataProvider' => $dataProvider,
         'filterModel'  => $searchModel,
         'columns'      => [
-//'id'
+'id',
+            'model'
             ]
         ]);
         ?>
