4762410e573f3eabaa689e54e201773926c3d421
aivavic <afanasjev-v@yandex.ru>
2015-12-25 18:32:05 +0200

    fix testdrive

--- frontend/assets/js/lib/verify.js
+++ frontend/assets/js/lib/verify.js
@@ -1882,8 +1882,21 @@
                     return "Use digits and spaces only";
                 if(v.match(/^\+/))
                     return true; //allow all international
-
+                if(!v.match(/^0/))
+                    return "Number must start with 0";
                 if(v.replace(/\s/g,"").length !== 7)
+                    return "Must be 7 digits long";
+                return true;
+            },
+            phone_code: function(r) {
+                r.val(r.val().replace(/\D/g,''));
+                var v = r.val();
+                if(!v.match(/^\+?[\d\s]+$/))
+                    return "Use digits and spaces only";
+                if(v.match(/^\+/))
+                    return true; //allow all international
+
+                if(v.replace(/\s/g,"").length !== 3)
                     return "Must be 7 digits long";
                 return true;
             },
