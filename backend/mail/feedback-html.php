c7501a282b6eb54e92379bc2a083ab5a398ea064
aivavic <afanasjev-v@yandex.ru>
2016-01-19 20:33:15 +0200

    fix feedback

--- backend/mail/feedback-html.php
+++ backend/mail/feedback-html.php
@@ -4,9 +4,9 @@ use yii\helpers\Html;
 
 <div class="password-reset">
 
-    <p>Здравствуйте, сообщение от <?= Html::encode($name) ?>,</p>
+    <p>Здравствуйте, сообщение от <?= $name; ?>,</p>
 
-    <p><?= Html::encode($message) ?></p>
+    <p><?= $message; ?></p>
 
     <p><?php echo date('l jS \of F Y h:i:s A', $date);?></p>
 </div>
