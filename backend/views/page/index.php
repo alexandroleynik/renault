5b1394ca22ffaba47eb396cf62f7d26a528e5e52
aivavic <afanasjev-v@yandex.ru>
2016-01-19 21:38:49 +0200

    fix feedback

--- backend/views/page/index.php
+++ backend/views/page/index.php
@@ -26,35 +26,92 @@ $this->registerJs($js);
 
     <a class="btn btn-default" href="/page/create"><?= Yii::t('backend', 'Create page'); ?></a>
 
-    <span class="dropdown">
-        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-            <?= Yii::t('backend', 'Clone page'); ?>
-            <span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">            
-            <?php
-            foreach ($list as $key => $value) {
-                echo '<li><a href="/page/create?locale_group_id=' . $key . '">' . $value . '</a></li>';
-            }
-            ?>
+    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#Modal1"><?= Yii::t('backend', 'Clone page'); ?></button>
 
-        </ul>
-    </span>
+    <div id="Modal1" class="modal fade" role="dialog">
+        <div class="modal-dialog">
 
-    <span class="dropdown">
-        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-            <?= Yii::t('backend', 'Extend page'); ?>
-            <span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-            <?php
-            foreach ($list as $key => $value) {
-                echo '<li><a href="/page/create?locale_group_id=' . $key . '&scenario=extend">' . $value . '</a></li>';
-            }
-            ?>
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title"><?= Yii::t('backend', 'Clone page'); ?></h4>
+                </div>
+                <div class="modal-body">
+                    <ul>
+                        <?php
+                        foreach ($list as $key => $value) {
+                            echo '<li><a href="/page/create?locale_group_id=' . $key . '">' . $value . '</a></li>';
+                        }
+                        ?>
 
-        </ul>
-    </span>
+                    </ul>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal"><?= Yii::t('backend', 'Close'); ?></button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+<!--    <span class="dropdown">-->
+<!--        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">-->
+<!--            --><?//= Yii::t('backend', 'Clone page'); ?>
+<!--            <span class="caret"></span>-->
+<!--        </button>-->
+<!--        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">            -->
+<!--            --><?php
+//            foreach ($list as $key => $value) {
+//                echo '<li><a href="/page/create?locale_group_id=' . $key . '">' . $value . '</a></li>';
+//            }
+//            ?>
+<!---->
+<!--        </ul>-->
+<!--    </span>-->
+
+
+    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#Modal1"><?= Yii::t('backend', 'Extend page'); ?></button>
+
+    <div id="Modal1" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title"><?= Yii::t('backend', 'Extend page'); ?></h4>
+                </div>
+                <div class="modal-body">
+                    <ul>
+                        <?php
+                        foreach ($list as $key => $value) {
+                            echo '<li><a href="/page/create?locale_group_id=' . $key . '&scenario=extend">' . $value . '</a></li>';
+                        }
+                        ?>
+
+                    </ul>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal"><?= Yii::t('backend', 'Close'); ?></button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+<!--    <span class="dropdown">-->
+<!--        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">-->
+<!--            --><?//= Yii::t('backend', 'Extend page'); ?>
+<!--            <span class="caret"></span>-->
+<!--        </button>-->
+<!--        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">-->
+<!--            --><?php
+//            foreach ($list as $key => $value) {
+//                echo '<li><a href="/page/create?locale_group_id=' . $key . '&scenario=extend">' . $value . '</a></li>';
+//            }
+//            ?>
+<!---->
+<!--        </ul>-->
+<!--    </span>-->
 
     <?php
     $columns = [
