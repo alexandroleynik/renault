e1bd0f05291eefda20b65c7c57c68d034bf1918b
aivavic <afanasjev-v@yandex.ru>
2016-01-19 17:41:38 +0200

    fix feedback

--- backend/views/feedback/view.php
+++ backend/views/feedback/view.php
@@ -1,12 +1,12 @@
 <?php
 $user = \api\models\User::findOne($model->domain_id);
 $user = $user ? $user->username : '';
-echo $model->id;
+echo 'Сообщение № ' . $model->id;
 ?>
 <br/>
-Сообщение от <?= $user; ?> ;
+<h4>Сообщение от <?= $user; ?> ;</h4>
 <br/>
 <h2><b><?= $model->subject; ?></b></h2>
-<?= $model->text; ?>
+<p><?= $model->text; ?></p>
     <br/>
 <?php //\yii\helpers\VarDumper::dump($model , 11, true);
