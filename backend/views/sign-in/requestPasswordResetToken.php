3cbe40b2220614445b7b5c784ba1c559ce263050
whiskas <sailorkoto@Selena.local>
2016-01-29 14:33:27 +0200

    admin login upd 4

--- backend/views/sign-in/requestPasswordResetToken.php
+++ backend/views/sign-in/requestPasswordResetToken.php
@@ -10,7 +10,11 @@ use yii\bootstrap\ActiveForm;
 $this->title = Yii::t('backend', 'Request password reset');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<div class="fullscreen-background">
+  <img src="/img/00080954.jpg" alt="background" />
+</div>
 <div class="site-request-password-reset">
+  <div class="login-box">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p><?=Yii::t('backend', 'Please fill out your email. A link to reset password will be sent there.') ?></p>
@@ -28,4 +32,5 @@ $this->params['breadcrumbs'][] = $this->title;
             <?php ActiveForm::end(); ?>
         </div>
     </div>
+  </div>
 </div>
--- backend/views/sign-in/resetPassword.php
+++ backend/views/sign-in/resetPassword.php
@@ -9,29 +9,23 @@ use yii\bootstrap\ActiveForm;
 
 $this->title = 'Reset password';
 $this->params['breadcrumbs'][] = $this->title;
-$this->params['body-class'] = 'reset-password-page';
 ?>
-<div class="fullscreen-background">
-  <img src="/img/00080954.jpg" alt="background" />
-</div>
 <div class="site-reset-password">
-  <div class="login-box">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p><?=Yii::t('backend', 'Please choose your new password:'); ?></p>
 
     <div class="row">
-      <div class="col-lg-5">
-        <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
 
-        <?= $form->field($model, 'password')->passwordInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'password')->passwordInput(['autofocus' => true]) ?>
 
-        <div class="form-group">
-          <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+                <div class="form-group">
+                    <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+                </div>
+
+            <?php ActiveForm::end(); ?>
         </div>
-
-        <?php ActiveForm::end(); ?>
-      </div>
     </div>
-  </div>
 </div>
