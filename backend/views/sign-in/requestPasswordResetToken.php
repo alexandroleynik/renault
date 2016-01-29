6e594541fd0b46943e6b2fee1b34ad349a1a0a9e
whiskas <sailorkoto@Selena.local>
2016-01-29 14:35:55 +0200

    admin login upd 5

--- backend/views/sign-in/requestPasswordResetToken.php
+++ backend/views/sign-in/requestPasswordResetToken.php
@@ -15,22 +15,24 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 <div class="site-request-password-reset">
   <div class="login-box">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="login-box-body">
+      <h1><?= Html::encode($this->title) ?></h1>
+      <p><?=Yii::t('backend', 'Please fill out your email. A link to reset password will be sent there.') ?></p>
 
-    <p><?=Yii::t('backend', 'Please fill out your email. A link to reset password will be sent there.') ?></p>
+      <div class="row">
+          <div class="col-lg-5">
+              <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
+                  <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
 
-                <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+                  <div class="form-group">
+                      <?= Html::submitButton(Yii::t('backend', 'Send'),
+                        ['class' => 'btn btn-primary btn-flat btn-block login-button']) ?>
+                  </div>
 
-                <div class="form-group">
-                    <?= Html::submitButton(Yii::t('backend', 'Send'), ['class' => 'btn btn-primary']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
-        </div>
+              <?php ActiveForm::end(); ?>
+          </div>
+      </div>
     </div>
   </div>
 </div>
