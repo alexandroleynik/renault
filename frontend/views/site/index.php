8bc991c14e51b0e975518a759a53fb4ea31a2ca3
whiskas <sailorkoto@Selena.local>
2016-02-02 11:33:50 +0200

    hide mob pop-up

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -55,7 +55,7 @@ foreach (Page::getMetaTags() as $tag) {
 //\frontend\assets\AppAsset::register($this);
 ?>
 
-<script>
+<!--<script>
 function mobilePopup() {
   var isMobile = false;
   if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
@@ -90,4 +90,4 @@ function mobilePopup() {
 }
 
 window.onload = function(){mobilePopup();};
-</script>
+</script>-->
