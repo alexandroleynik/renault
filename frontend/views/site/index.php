18f57bfe65ab8081f5d65692990c8d143df1d7f1
whiskas <sailorkoto@Selena.local>
2016-02-03 19:16:52 +0200

    breadcrumbs upd 0.18

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -56,10 +56,12 @@ foreach (Page::getMetaTags() as $tag) {
   var cookie;
   cookie = getCookie('mobileFlag');
   console.log('cookie: ' + cookie);
+  console.log('cookie: ' + document.cookie);
 
   var date = new Date(new Date().getTime() + (10 * 365 * 24 * 60 * 60));
   document.cookie = 'mobileFlag=true;path=/;expires=' + date.toUTCString();
 
   cookie = getCookie('mobileFlag');
   console.log('cookie: ' + cookie);
+  console.log('cookie: ' + document.cookie);
 </script>
