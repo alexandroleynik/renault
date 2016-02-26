c9ec74d4d75eb278907dda63b5a2f146f2e5f882
aivavic <afanasjev-v@yandex.ru>
2016-02-26 14:27:53 +0200

    import

--- common/components/excell/ImportBehavior.php
+++ common/components/excell/ImportBehavior.php
@@ -2,81 +2,120 @@
 /**
  * Created by PhpStorm.
  * User: viktor
-
  */
 namespace common\components\excell;
+
+use common\models\Model;
 use Yii;
 use yii\base\Behavior;
 use yii\validators\FileValidator;
 use yii\web\UploadedFile;
 use common\models\Price;
-class ImportBehavior extends Behavior {
+
+class ImportBehavior extends Behavior
+{
     const XLS_FILE = 'excel_file';
     public $defaultFormat = 'Excel5';
     public $excel_file;
     public $onImportRow = 'onImportRow';
     private $_uploaded_file;
     private $_log_data_provider = [];
-    private function upload_file($fileAttribute = self::XLS_FILE, array $extensions = null, $maxSize = 10485760 ){
-        $this->_uploaded_file = (new UploadedFile())->getInstance($this->owner, $fileAttribute );
-        if($this->_uploaded_file ){
+
+    private function upload_file($fileAttribute = self::XLS_FILE, array $extensions = null, $maxSize = 10485760)
+    {
+        $this->_uploaded_file = (new UploadedFile())->getInstance($this->owner, $fileAttribute);
+        if ($this->_uploaded_file) {
             $fileValidator = new FileValidator([
-                'extensions' => is_array($extensions) ? $extensions : ['xls','xlsx'],
+                'extensions' => is_array($extensions) ? $extensions : ['xls', 'xlsx'],
                 'maxSize' => $maxSize
             ]);
-            if( $fileValidator->validate( $this->_uploaded_file, $error ) ){
+            if ($fileValidator->validate($this->_uploaded_file, $error)) {
                 return true;
             } else $this->owner->addError($fileAttribute, $error);
-        } else if(isset($_FILES['Dvk'])) {
-            $this->owner->addError($fileAttribute, Yii::t('app', 'Choose file for import!') );
+        } else if (isset($_FILES['Dvk'])) {
+            $this->owner->addError($fileAttribute, Yii::t('app', 'Choose file for import!'));
         }
         return false;
     }
-    private function import_row( $data = [] ){
-        if( is_string( $this->onImportRow ) && method_exists($this->owner, $this->onImportRow) ){
-            return call_user_func_array ([ $this->owner, $this->onImportRow ], $data);
-        } else if( $this->onImportRow instanceof \Closure ){
-            return call_user_func_array ($this->onImportRow, $data);
+
+    private function import_row($data = [])
+    {
+        if (is_string($this->onImportRow) && method_exists($this->owner, $this->onImportRow)) {
+            return call_user_func_array([$this->owner, $this->onImportRow], $data);
+        } else if ($this->onImportRow instanceof \Closure) {
+            return call_user_func_array($this->onImportRow, $data);
         }
         return false;
     }
+
     private $_current_row = 0;
-    public function importExcel($function=null){
-        if( $this->upload_file() ){
+
+    public function importExcel($function = null)
+    {
+        if ($this->upload_file()) {
             Price::deleteAll();
-            if( $function ){
+            if ($function) {
                 $this->onImportRow = $function;
             }
             //$reader = \PHPExcel_IOFactory::createReader( /*$this->defaultFormat*/ );
-            $objPHPExcel = \PHPExcel_IOFactory::load( $this->_uploaded_file->tempName );
-            //$objPHPExcel->setActiveSheetIndex(0);
+            $objPHPExcel = \PHPExcel_IOFactory::load($this->_uploaded_file->tempName);
             $objWorksheet = $objPHPExcel->getActiveSheet();
             $highestRow = $objWorksheet->getHighestRow();
+            $result = [];
+            $foo = [];
             foreach ($objWorksheet->getRowIterator() as $i => $row) {
                 $cellIterator = $row->getCellIterator();
                 $cellIterator->setIterateOnlyExistingCells(false);
                 $row = [];
-
                 foreach ($cellIterator as $cell) $row[] = $cell->getValue();
-//                \yii\helpers\VarDumper::dump($row, 9, 9); die();
+                $foo = [$row[0] => $row[4]];
+                if (!array_key_exists($row[0], $result)) {
+                    $result = array_merge($result, $foo);
+                }
+            }
+            foreach($result as $key => $field){
+
+                if(isset($field) and isset($key)){
+                    $models = Model::findAll(['title' => $key]);
+                    if($models){
+                        foreach($models as $model){
+                            $model->price = $field;
+                            $model->update();
+                        }
+                    }
+                }
+
+//                \yii\helpers\VarDumper::dump($model, 9, 9);
+            }
+
+            foreach ($objWorksheet->getRowIterator() as $i => $row) {
+                $cellIterator = $row->getCellIterator();
+                $cellIterator->setIterateOnlyExistingCells(false);
+                $row = [];
+                foreach ($cellIterator as $cell) $row[] = $cell->getValue();
+                \yii\helpers\VarDumper::dump($row, 9, 9);
                 $this->_current_row = $i;
-//                \yii\helpers\VarDumper::dump($row, 9, 9);
-                if( $this->import_row([
+                if ($this->import_row([
                     'row' => $row,
 
                     'index' => $i,
                     'max_row' => $highestRow
-                ]) ) {
+                ])
+                ) {
                 } else {
                     break;
                 }
             }
         }
     }
-    public function getImportLog(){
+
+    public function getImportLog()
+    {
         return $this->_log_data_provider;
     }
-    public function addLog($msg) {
-        $this->_log_data_provider[] = $this->_current_row.': '.$msg;
+
+    public function addLog($msg)
+    {
+        $this->_log_data_provider[] = $this->_current_row . ': ' . $msg;
     }
 }
--- frontend/assets/js/all/widgets/arrays/objects/characteristics.js
+++ frontend/assets/js/all/widgets/arrays/objects/characteristics.js
@@ -19,6 +19,59 @@ app.view.wfn['characteristics'] = (function () {
         
         var data = widget;
         data.t = app.view.getTranslationsFromData(data);
+
+        //var params = {
+        //    "fields": 'id,slug,title,description,thumbnail_base_url,thumbnail_path,description,video_base_url,video_path',
+        //    "per-page": data.count,
+        //    "sort": sort,
+        //    "where": {
+        //        locale: app.config.frontend_app_locale,
+        //        "domain_id": app.config.frontend_app_domain_id,
+        //    }
+        //
+        //};
+        //
+        //$.getJSON(
+        //    app.config.frontend_app_api_url + '/db/articles',
+        //    params,
+        //    function (articlesData) {
+        //        //process domain articles
+        //        if (articlesData.items[0]) {
+        //            $.extend(data, articlesData);
+        //
+        //            $.each(data.items, function (key, val) {
+        //
+        //                data.items[key].previewImg = val.thumbnail_base_url + '/' + val.thumbnail_path;
+        //                data.items[key].viewUrl = app.view.helper.preffix + '/news/' + val.slug;
+        //            });
+        //
+        //            data.urlToNews = app.view.helper.preffix + '/news';
+        //
+        //            loadTemplate(data);
+        //        }
+        //
+        //        //get default articles
+        //        if (!articlesData.items[0]) {
+        //            params.where.domain_id = app.config.frontend_app_default_domain_id;
+        //
+        //            $.getJSON(
+        //                app.config.frontend_app_api_url + '/db/articles',
+        //                params,
+        //                function (articlesData) {
+        //                    $.extend(data, articlesData);
+        //
+        //                    $.each(data.items, function (key, val) {
+        //
+        //                        data.items[key].previewImg = val.thumbnail_base_url + '/' + val.thumbnail_path;
+        //                        data.items[key].viewUrl = app.view.helper.preffix + '/news/' + val.slug;
+        //                    });
+        //
+        //                    data.urlToNews = app.view.helper.preffix + '/news';
+        //
+        //                    loadTemplate(data);
+        //                });
+        //        }
+        //    });
         loadTemplate(data);
     }
 
