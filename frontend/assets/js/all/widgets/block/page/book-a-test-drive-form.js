3b6ce1185e2bf7d6f6bc08e7ac07f6ca7f7b90af
Alexandr <alexandr@oleynik.org>
2016-03-03 13:06:30 +0200

    Update book-a-test-drive-form.js

--- frontend/assets/js/all/widgets/block/page/book-a-test-drive-form.js
+++ frontend/assets/js/all/widgets/block/page/book-a-test-drive-form.js
@@ -181,7 +181,7 @@ app.view.wfn['book-a-test-drive-form'] = (function () {
           maxZoom: 7,
           gridSize: 50,
           styles: [{
-            height: 80,
+            height: 60,
             width: 40,
             anchor: [20,0],
             textColor: '#fff',
