d2f08440ef76c291394f00a9c0e0839dc1c23b0c
Alexandr <alexandr@oleynik.org>
2016-03-10 13:54:33 +0200

    Update SiteController.php

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -59,103 +59,103 @@ class SiteController extends Controller
     public function actionAjax_curl()
     {
         $data=array( // заполненый массив с тестовыми данными
-			'Fields' => array(
-				'0' => array (
-					'key' => 'RenaultDealerId',
-					'value' => 35,
-				),
-				'1' => array (
-					'key' => 'CategoryId',
-					'value' => 3,
-				),
-				'2' => array (
-					'key' => 'DealerId',
-					'value' => 43,
-				),
-				'3' => array (
-					'key' => 'RenaultDealerDomain',
-					'value' => 'Источник лида: dealers.renault.ua',
-				),
-				'4' => array (
-					'key' => 'LastName',
-					'value' => 'Тест',
-				),
-				'5' => array (
-					'key' => 'FirstName',
-					'value' => 'Тест',
-				),
-				'6' => array (
-					'key' => 'Patronymic',
-					'value' => 'Тест',
-				),
-				'7' => array (
-					'key' => 'BirthDate',
-					'value' => '24.04.1992',
-				),
-				'8' => array (
-					'key' => 'VehicleModel',
-					'value' => 'Capture',
-				),
-				'9' => array (
-					'key' => 'eMail',
-					'value' => 'test@test.com',
-				),
-				'10' => array (
-					'key' => 'DaytimePhoneNumber',
-					'value' => '+38(063)0000000',
-				),
-				'11' => array (
-					'key' => 'TestDriveSuggestions',
-					'value' => '09.03.2016',
-				),
-				'12' => array (
-					'key' => 'TestDriveSuggestions',
-					'value' => '9:00-10:00',
-				),
-				'13' => array (
-					'key' => 'UsingPersonalInfo',
-					'value' => 'yes',
-				),
-				'14' => array (
-					'key' => 'CampaignUniqueId',
-					'value' => '...',
-				),
-				'15' => array (
-					'key' => 'Media',
-					'value' => 'http://dealers.renault.com.ua',
-				),
-				'16' => array (
-					'key' => 'ContactByPhone',
-					'value' => 'true',
-				),
-				'17' => array (
-					'key' => 'ContactByMail',
-					'value' => 'true',
-				)
-			),
-			'Token' => 'String content',
-		);
-		
-// 		$url = "https://lmt-ua.makolab.net/LMTService.svc/rest/SaveLeadJson"; // путь к лмт
-// 		$data=json_encode($data); // json формат массива
-
-//         // сам курл запроса
-// 		$curl = curl_init($url);
-// 		curl_setopt($curl, CURLOPT_HEADER, false);
-// 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-// 		curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
-// 		curl_setopt($curl, CURLOPT_POST, true);
-// 		curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-// 		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-// 		$json_response = curl_exec($curl);
-// 		curl_close($curl);
-		
-
-
-// 		$xml = new SimpleXMLElement($json_response); // примем ответа от сервера
-// 		$bla = $xml->ErrorCode; // получение кода ошиби ну или ответа
-		
-		var_dump($data);
+	'Fields' => array(
+		'0' => array (
+    			'key' => 'RenaultDealerId',
+    			'value' => 34,
+    		),
+    		'1' => array (
+    			'key' => 'CategoryId',
+    			'value' => 3,
+    		),
+    		'2' => array (
+    			'key' => 'DealerId',
+    			'value' => 27,
+    		),
+		'3' => array (
+			'key' => 'RenaultDealerDomain',
+			'value' => 'Источник лида: dealers.renault.ua',
+		),
+		'4' => array (
+			'key' => 'LastName',
+			'value' => 'Тест',
+		),
+		'5' => array (
+			'key' => 'FirstName',
+			'value' => 'Тест',
+		),
+		'6' => array (
+			'key' => 'Patronymic',
+			'value' => 'Тест',
+		),
+		'7' => array (
+			'key' => 'BirthDate',
+			'value' => '24.04.1992',
+		),
+		'8' => array (
+			'key' => 'VehicleModel',
+			'value' => 'Capture',
+		),
+		'9' => array (
+			'key' => 'eMail',
+			'value' => 'test@test.com',
+		),
+		'10' => array (
+			'key' => 'DaytimePhoneNumber',
+			'value' => '+38(063)0000000',
+		),
+		'11' => array (
+			'key' => 'TestDriveSuggestions',
+			'value' => '09.03.2016',
+		),
+		'12' => array (
+			'key' => 'TestDriveSuggestions',
+			'value' => '9:00-10:00',
+		),
+		'13' => array (
+			'key' => 'UsingPersonalInfo',
+			'value' => 'yes',
+		),
+		'14' => array (
+			'key' => 'CampaignUniqueId',
+			'value' => '...',
+		),
+		'15' => array (
+			'key' => 'Media',
+			'value' => 'http://dealers.renault.com.ua',
+		),
+		'16' => array (
+			'key' => 'ContactByPhone',
+			'value' => 'true',
+		),
+		'17' => array (
+			'key' => 'ContactByMail',
+			'value' => 'true',
+		)
+		),
+		'Token' => 'String content',
+	);
+	
+	// 	$url = "https://lmt-ua.makolab.net/LMTService.svc/rest/SaveLeadJson"; // путь к лмт
+	// 	$data=json_encode($data); // json формат массива
+	
+	// сам курл запроса
+	// 	$curl = curl_init($url);
+	// 	curl_setopt($curl, CURLOPT_HEADER, false);
+	// 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	// 	curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
+	// 	curl_setopt($curl, CURLOPT_POST, true);
+	// 	curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+	// 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	// 	$json_response = curl_exec($curl);
+	// 	curl_close($curl);
+	
+	
+	
+	// 	$xml = new SimpleXMLElement($json_response); // примем ответа от сервера
+	// 	$bla = $xml->ErrorCode; // получение кода ошиби ну или ответа
+	
+	var_dump($data);
 		
     }
 
