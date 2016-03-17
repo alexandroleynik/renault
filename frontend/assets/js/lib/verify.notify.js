55ee893ab933bf1d7b2569cb801f928419857b80
Alexandr <alexandr@oleynik.org>
2016-03-17 13:30:31 +0200

    Update verify.notify.js

--- frontend/assets/js/lib/verify.notify.js
+++ frontend/assets/js/lib/verify.notify.js
@@ -2419,6 +2419,54 @@
             },
             name: function(r) {
                 var v = r.val();
+                
+                function check_three(v){
+                    var c = v.length, i = 0, max3l = 0;
+                    for (i; i < c; i++) {
+            			if (i < (c-2)) {
+            				L = v.charAt(i).toUpperCase();
+            				p = i + 1;
+            				if (p>0) {
+            					if (L == v.charAt(p).toUpperCase()) {
+            						pp = p + 1;
+            						if (pp>0) {
+            							if (L == v.charAt(pp).toUpperCase()) {
+            								max3l = 1;
+            							}
+            						}
+            					}
+            				}	
+            			} else {
+            				for (j = c; j >= i; j--) {
+            					L = v.charAt(j).toUpperCase();
+            					p = j - 1;
+            					if (p>0) {
+            						if (L == v.charAt(p).toUpperCase()) {
+            							pp = p - 1;
+            							if (pp>0) {
+            								if (L == v.charAt(pp).toUpperCase()) {
+            									max3l = 1;
+            								}
+            							}
+            						}
+            					}
+            				};
+            			}
+            			
+            		}
+            		if (max3l == 1) {
+            			return false;
+            		} else {
+            		    return true;
+            		}
+                }
+                if(!check_three(v)){
+                    console.log('fails2');
+                    //$(".submit-form-button").attr('disabled','disabled');
+                    //$(".submit-form-button").addClass('btn-disabled');
+                    return app.router.locale == "uk"?"3 літери":"3 буквьі";
+                }
+                
                 if(!v.match(/^\+?[а-яіїєґ\s,А-ЯІЇЄҐ\s]+$/)){
                     console.log('fails2');
                     //$(".submit-form-button").attr('disabled','disabled');
@@ -2488,8 +2536,6 @@
             		    return true;
             		}
                 }
-                
-                
                 if(!check_three(v)){
                     console.log('fails2');
                     //$(".submit-form-button").attr('disabled','disabled');
@@ -2524,6 +2570,54 @@
             },
             patronymic: function(r) {
                 var v = r.val();
+                
+                function check_three(v){
+                    var c = v.length, i = 0, max3l = 0;
+                    for (i; i < c; i++) {
+            			if (i < (c-2)) {
+            				L = v.charAt(i).toUpperCase();
+            				p = i + 1;
+            				if (p>0) {
+            					if (L == v.charAt(p).toUpperCase()) {
+            						pp = p + 1;
+            						if (pp>0) {
+            							if (L == v.charAt(pp).toUpperCase()) {
+            								max3l = 1;
+            							}
+            						}
+            					}
+            				}	
+            			} else {
+            				for (j = c; j >= i; j--) {
+            					L = v.charAt(j).toUpperCase();
+            					p = j - 1;
+            					if (p>0) {
+            						if (L == v.charAt(p).toUpperCase()) {
+            							pp = p - 1;
+            							if (pp>0) {
+            								if (L == v.charAt(pp).toUpperCase()) {
+            									max3l = 1;
+            								}
+            							}
+            						}
+            					}
+            				};
+            			}
+            			
+            		}
+            		if (max3l == 1) {
+            			return false;
+            		} else {
+            		    return true;
+            		}
+                }
+                if(!check_three(v)){
+                    console.log('fails2');
+                    //$(".submit-form-button").attr('disabled','disabled');
+                    //$(".submit-form-button").addClass('btn-disabled');
+                    return app.router.locale == "uk"?"3 літери":"3 буквьі";
+                }
+                
                 if(!v.match(/^\+?[а-яіїєґ,А-ЯІЇЄҐ]+$/)){
                     console.log('fails2');
                     $(".submit-form-button").attr('disabled','disabled');
