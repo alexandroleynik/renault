14ed73e1e30f3bdce399f35e7bd90a8e3129c50e
allhaze <allhaze.team@gmail.com>
2016-04-14 10:47:17 +0300

    loadmore for dropdown

--- backend/web/js/app.js
+++ backend/web/js/app.js
@@ -9,5 +9,64 @@ $(function() {
         forcePlaceholderSize: true,
         zIndex: 999999
     }).disableSelection();
+    
     $(".connectedSortable .box-header, .connectedSortable .nav-tabs-custom").css("cursor", "move");
+
+    window.loadMore = function(element, event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var pointer = parseInt($(element).attr('data-pointer')), currentPointer, $parent = $(element).parent();
+
+        $(element).remove();
+
+        $parent.find('li').each(function(index) {
+
+            currentPointer = parseInt($(this).attr('data-pointer'));
+
+            if(currentPointer > pointer && currentPointer < (pointer + 11)) {
+                $(this).css('display', 'block');
+            }
+        });
+
+    };
+    $('ul.dropdown-menu').each(function(index) {
+        var $li = $(this).find('li'),
+            length = $li.length,
+            restriction = 10;
+
+        if(length > restriction) {
+
+            $(this).css({
+                'maxHeight': '320px',
+                'overflow': 'auto'
+            });
+
+            var pointer = 0,
+                pointerLoad = 0, extraCss = 'background: #f6f6f6;cursor:pointer;';
+            for(var i = length - 1; i >= 0; i --) {
+                if(pointer != 0 && !(pointer % restriction)) {
+
+                    if(pointerLoad > 1) {
+                        extraCss += 'display:none';
+                    }
+
+                    $($li[pointer]).after('<li onclick="loadMore(this, event)" data-pointer="' + pointer + '" data-load="'+ pointerLoad +'" style="'+ extraCss +'">> Load More</li>');
+                }
+
+                if(pointer > restriction) {
+                    $($li[pointer]).css('display', 'none');
+                    $($li[pointer]).attr('data-load-pos', pointerLoad);
+                }
+
+                if(pointer != 0 && !((pointer - 1) % restriction)) {
+                    pointerLoad++;
+                }
+
+                $($li[pointer]).attr('data-pointer', pointer);
+
+                pointer++;
+            }
+        }
+    });
 })
